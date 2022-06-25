b0ab2d2176c466300f611a17d90443698b5f53c7
unknown <Mallerly Rojas Cardenas>
2022-06-25 08:52:09 -0500

    en esta clase se realizo un ciclo que cuenta los numeros de 2 en 2

---
+++ src/NumerosDeDosEnDos.java
@@ -0,0 +1,10 @@
+public class NumerosDeDosEnDos {
+    public static void main(String[] args) {
+        //imprimir 10 numeros sumados de 2 en 2
+
+        for (int i =2; i <=20; i= i+2){
+            System.out.println(" numero: " + i);
+        }
+    }
+
+}
