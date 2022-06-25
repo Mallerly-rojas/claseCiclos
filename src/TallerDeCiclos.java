5ebe4e5f7e4f07ddc79759edab448918288bf571
unknown <Mallerly Rojas Cardenas>
2022-06-25 09:15:31 -0500

    ciclo que imprime 1 3 1 3 1 3

--- src/TallerDeCiclos.java
+++ src/TallerDeCiclos.java
@@ -1,5 +1,11 @@
 public class TallerDeCiclos {
     public static void main(String[] args) {
 
+        for (int i =0; i<=2; i++){
+            System.out.println("1");
+            System.out.println("3");
+        }
+
+
     }
 }
