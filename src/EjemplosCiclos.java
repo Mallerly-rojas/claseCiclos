db34dd7b153cd77460250e2db72991e2e57ed266
unknown <Mallerly Rojas Cardenas>
2022-06-25 08:28:01 -0500

    ejemplo en numeros

--- src/EjemplosCiclos.java
+++ src/EjemplosCiclos.java
@@ -1,5 +1,11 @@
+import javax.swing.*;
+
 public class EjemplosCiclos {
     public static void main(String[] args) {
+        for (int i = 0;i < 3; i++){
+            JOptionPane.showMessageDialog( null, "hola mundo");
+
+        }
 
     }
 
